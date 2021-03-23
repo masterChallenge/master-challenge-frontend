c1f0eb7b440d1013df4d5d669548838294ff13fa
Mario Silva <hmariosilvae@gmail.com>
2021-03-23 12:17:43 -0600

    [update] navbar links

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../../assets/Logo-Horizontal-BW.svg';
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../../assets/Logo-Horizontal-BW.svg";
 
 const Navbar = () => {
   return (
-    <header className='w-full bg-primary flex justify-between px-24 items-center h-24 shadow-lg'>
-      <Link to='/'>
-        <Logo alt='Logo' />
+    <header className="w-full bg-primary flex justify-between px-24 items-center h-24 shadow-lg">
+      <Link to="/">
+        <Logo alt="Logo" />
       </Link>
-      <nav className='text-white font-bold'>
-        <ul className='flex'>
-          <li className='m-6'>
-            <Link to='/home'>Challenges</Link>
+      <nav className="text-white font-bold">
+        <ul className="flex">
+          <li className="m-6">
+            <Link to="/home">Challenges</Link>
           </li>
-          <li className='m-6'>
-            <Link to='https://wiki.master-challenge.me/'>Documentation</Link>
+          <li className="m-6">
+            <a
+              target="_blank"
+              without
+              rel="noreferrer"
+              href="https://wiki.master-challenge.me/"
+            >
+              Documentation
+            </a>
           </li>
-          <li className='m-6'>
-            <Link to='https://airtable.com/shr5cd3oViPHxHpkv'>
+          <li className="m-6">
+            <a
+              target="_blank"
+              without
+              rel="noreferrer"
+              href="https://airtable.com/shr5cd3oViPHxHpkv"
+            >
               Send your feedback
-            </Link>
+            </a>
           </li>
         </ul>
       </nav>
