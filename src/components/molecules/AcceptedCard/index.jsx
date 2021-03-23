4374835471574314aa95646aab8ed909e98c9265
Djkde <sergio.estrella@utp.edu.co>
2021-03-23 17:56:11 -0500

    [FIX] AcceptedCard display bug

--- src/components/molecules/AcceptedCard/index.jsx
+++ src/components/molecules/AcceptedCard/index.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useContext } from "react";
 import { ReactComponent as Accepted } from "../../../assets/Completed.svg";
 import Button from "../../atoms/Button/";
+import ModalContext from "../../../context/ModalContext";
 
 const AcceptedCard = ({ pid, ...props }) => {
+  const { showModal, setShowModal } = useContext(ModalContext);
+
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div className="bg-primary flex flex-col items-center text-center rounded-3xl px-20 py-6 shadow-lg text-secondary-darker">
       <Accepted />
@@ -12,11 +18,11 @@ const AcceptedCard = ({ pid, ...props }) => {
         <p>You're getting better at css</p>
       </span>
       <a href={`/challenge/${parseInt(pid) + 1}`}>
-        <Button color="secondary-dark" type="primary">
+        <Button color="secondary-dark" type="primary" onClick={handleClick}>
           Next Challenge
         </Button>
       </a>
-      <Link to="/home" className="mt-4 text-sm font-bold">
+      <Link to="/home" className="mt-4 text-sm font-bold" onClick={handleClick}>
         Return to dashboard
       </Link>
     </div>
